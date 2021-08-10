3cf4c25a2730509f882f34841ea2508c3e923220
Himani Patel <62655342+Himanipatell@users.noreply.github.com>
2021-08-10 16:19:23 +0530

    adding proper format

---
+++ Image Compression/Models/ImageModel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Image_Compression.Models
+{
+    public class ImageModel
+    {
+        public List<string> images { get; set; }
+        public string imageByteArray { get; set; }
+        public string watermarkpath { get; set; }
+        public float widthFraction { get; set; }
+        public float heightFraction { get; set; }
+        public string watermarkText { get; set; }
+    }
+}
+
